2cc921b189cadf24c8ee1700571b59c29f2e06fd
Mckervin Ceme <mckervinc@live.com>
2020-02-03 16:04:33 -0500

    show bundle size on build

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import external from "rollup-plugin-peer-deps-external";
 import resolve from "rollup-plugin-node-resolve";
 import url from "rollup-plugin-url";
 import postcss from "rollup-plugin-postcss";
+import bundleSize from "rollup-plugin-bundle-size";
 
 import pkg from "./package.json";
 
@@ -31,6 +32,7 @@ export default {
       exclude: "node_modules/**"
     }),
     resolve(),
-    commonjs()
+    commonjs(),
+    bundleSize()
   ]
 };
