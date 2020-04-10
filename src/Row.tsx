8a52cd4e5124d3e76879a3a4cef588f2f225bc16
Mckervin Ceme <mckervinc@live.com>
2020-04-10 16:54:36 -0400

    memoized cell

--- src/Row.tsx
+++ src/Row.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useContext, useLayoutEffect, useRef } from "react";
-import { ColumnProps, RowProps } from "../index";
+import { ColumnProps, RowProps, Generic, CacheFunction } from "../index";
 import { TableContext } from "./TableContext";
 
 //@ts-ignore TS2307
@@ -7,6 +7,52 @@ import Minus from "./svg/minus-circle.svg";
 
 //@ts-ignore TS2307
 import Plus from "./svg/plus-circle.svg";
+
+interface MemoizedCell {
+  row: Generic;
+  index: number;
+  width?: number;
+  column: ColumnProps;
+  isExpanded: boolean;
+  clearSizeCache: CacheFunction;
+  onExpanderClick: Function;
+}
+
+const MemoizedCell = React.memo(
+  ({ row, index, width, column, isExpanded, clearSizeCache, onExpanderClick }: MemoizedCell) => {
+    // cell renderer
+    const cellRenderer = (c: ColumnProps) => {
+      if (c.expander) {
+        let Logo: any = c.expander;
+        if (typeof c.expander === "boolean") {
+          Logo = isExpanded ? Minus : Plus;
+          return <Logo className="expander" onClick={onExpanderClick} />;
+        }
+
+        // assume the expander is a component
+        return <Logo isExpanded={isExpanded} onClick={onExpanderClick} />;
+      }
+
+      if (!c.cell) {
+        return row[c.key] || null;
+      }
+
+      const CustomCell = c.cell;
+      return <CustomCell row={row} index={index} clearSizeCache={clearSizeCache} />;
+    };
+
+    const style = {
+      width: width ? `${width}px` : undefined,
+      minWidth: width ? `${width}px` : undefined
+    };
+
+    return (
+      <div className="cell" style={style}>
+        {cellRenderer(column)}
+      </div>
+    );
+  }
+);
 
 const Row = ({
   row,
@@ -41,58 +87,34 @@ const Row = ({
   const subProps = { row, index, isExpanded, clearSizeCache };
 
   // function(s)
-  const onExpanderClick = () => {
+  const onExpanderClick = useCallback(() => {
     dispatch({ type: "updateExpanded", key: generateKeyFromRow(row, index) });
     expandedCalledRef.current = true;
-  };
-
-  // cell renderer
-  const cellRenderer = (c: ColumnProps) => {
-    if (c.expander) {
-      let Logo: any = c.expander;
-      if (typeof c.expander === "boolean") {
-        Logo = isExpanded ? Minus : Plus;
-        return <Logo className="expander" onClick={onExpanderClick} />;
-      }
-
-      // assume the expander is a component
-      return <Logo isExpanded={isExpanded} onClick={onExpanderClick} />;
-    }
-
-    if (!c.cell) {
-      return row[c.key] || null;
-    }
-
-    const Cell = c.cell;
-    return <Cell row={row} index={index} clearSizeCache={clearSizeCache} />;
-  };
+  }, [dispatch, row, index, generateKeyFromRow, expandedCalledRef]);
 
   const resetHeight = useCallback(() => {
-    if (!rowRef.current) {
+    if (!rowRef.current || !pixelWidths.length) {
       return;
     }
 
-    if (height !== calculateHeight(rowRef.current, index)) {
+    const computed = calculateHeight(rowRef.current, index);
+    if (height !== computed) {
       clearSizeCache(index);
     }
-  }, [rowRef, index, height, calculateHeight, clearSizeCache]);
+  }, [rowRef, index, height, calculateHeight, clearSizeCache, pixelWidths]);
 
   // effects
   // on expansion, clear the cache
-  useLayoutEffect(() => {
-    if (expandedCalledRef.current) {
-      clearSizeCache(index, true);
-    }
-  }, [isExpanded, index, clearSizeCache, expandedCalledRef]);
-
   // every time isExpanded/pixelWidth changes, check the height
   useLayoutEffect(() => {
     if (!expandedCalledRef.current) {
       resetHeight();
+    } else {
+      clearSizeCache(index, true);
     }
 
     expandedCalledRef.current = false;
-  }, [isExpanded, expandedCalledRef, resetHeight, pixelWidths]);
+  }, [isExpanded, expandedCalledRef, resetHeight, index, clearSizeCache]);
 
   return (
     <div
@@ -103,18 +125,18 @@ const Row = ({
       style={{ ...style, width: useRowWidth ? style.width : undefined }}
     >
       <div className="row-container" style={{ height: containerHeight }}>
-        {columns.map((c: ColumnProps, i: number) => {
-          const width = pixelWidths[i];
-          const style = {
-            width: width ? `${width}px` : undefined,
-            minWidth: width ? `${width}px` : undefined
-          };
-          return (
-            <div className="cell" key={`${uuid}-${c.key}-${key}`} style={style}>
-              {cellRenderer(c)}
-            </div>
-          );
-        })}
+        {columns.map((c: ColumnProps, i: number) => (
+          <MemoizedCell
+            key={`${uuid}-${c.key}-${key}`}
+            row={row}
+            column={c}
+            index={index}
+            width={pixelWidths[i]}
+            isExpanded={isExpanded}
+            clearSizeCache={clearSizeCache}
+            onExpanderClick={onExpanderClick}
+          />
+        ))}
       </div>
       {!SubComponent ? null : (
         <div className={isExpanded ? undefined : "hidden"}>
