1cb66aa509e8a29ac51aba4138a6c8a1919b9598
Cole Marsteller <cole@colemars.dev>
2020-04-15 20:49:35 -0500

    test out

--- src/RowWrapper.tsx
+++ src/RowWrapper.tsx
@@ -5,15 +5,20 @@ import Row from "./Row";
 interface Props {
   index: number;
   data: any;
+  CustomRow: React.FC;
 }
 
-const RowWrapper = React.memo(({ data, index, ...rest }: Props) => {
+const RowWrapper = React.memo(({ data, index, CustomRow, ...rest }: Props) => {
   const dataIndex = index - 1; // the header is at index 0
+
+  console.log(data, index);
 
   const { rows, ...metaData } = data;
   const row = rows[dataIndex];
 
-  return !row ? null : <Row row={row} index={dataIndex} {...rest} {...metaData} />;
+  if (!row) return null;
+
+  return CustomRow ? <CustomRow row={row} index={dataIndex} {...rest} {...metaData} /> : <Row row={row} index={dataIndex} {...rest} {...metaData} />;
 }, areEqual);
 
 export default RowWrapper;
