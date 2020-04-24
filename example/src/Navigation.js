18498ffff2cd5c01b722631e1f6f202016243a84
Mckervin Ceme <mckervinc@live.com>
2020-04-24 18:44:49 -0400

    add basic mobile nav

---
+++ example/src/Navigation.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Icon } from "semantic-ui-react";
+
+const StyledNav = styled.nav`
+  position: sticky;
+  top: 0;
+  width: 100%;
+  background-color: #1b1c1d;
+  color: rgba(255, 255, 255, 0.9);
+  padding: 1rem 10px;
+
+  @media screen and (min-width: 769px) {
+    display: none;
+  }
+`;
+
+const Toggle = styled(Icon)`
+  cursor: pointer;
+  display: block;
+`;
+
+const Navigation = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onClick = () => {
+    setIsOpen(!isOpen);
+    const elem = document.getElementById("pusher");
+    const side = document.getElementById("sidebar");
+    if (!elem || !side) {
+      return;
+    }
+
+    if (isOpen) {
+      side.style.overflow = "";
+      elem.style.overflow = "";
+    } else {
+      side.style.overflow = "hidden";
+      elem.style.overflow = "hidden";
+    }
+  };
+
+  return (
+    <StyledNav>
+      <Toggle name={isOpen ? "times" : "bars"} onClick={onClick} />
+      {!isOpen ? null : <div>{children}</div>}
+    </StyledNav>
+  );
+};
+
+export default Navigation;
